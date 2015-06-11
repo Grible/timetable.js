63dc421b1142f63e9e05b048526061d9dc63d491
steve <ssaanen@gmail.com>
2015-06-11 16:18:23 +0200

    added some simple functionality for setting data to timetable

--- app/scripts/plugin.js
+++ app/scripts/plugin.js
@@ -1,7 +1,55 @@
+/*jshint -W079*/
+
 'use strict';
 
-var Timetable = Timetable || function() {
-	this.message = 'hello world!';
+var Timetable = function() {
+	this.scope = {
+		hourStart: 9,
+		hourEnd: 17
+	};
+	this.locations = [];
 };
 
-Timetable.prototype = {};
+Timetable.prototype = {
+	setScope: function(start, end) {
+		function isValidHour(number) {
+			return isInt(number) && isInHourRange(number);
+		}
+		function isInt(number) {
+			return number === parseInt(number, 10);
+		}
+		function isInHourRange(number) {
+			return number >= 0 && number <= 24;
+		}
+
+		if (isValidHour(start) && isValidHour(end) && start < end) {
+			this.scope.hourStart = start;
+			this.scope.hourEnd = end;
+		} else {
+			throw new RangeError('Timetable scope should consist of (start, end) in whole hours between 0 and 24');
+		}
+
+		return this;
+	},
+	addLocations: function(newLocations) {
+		function hasProperFormat() {
+			return newLocations instanceof Array;
+		}
+
+		var existingLocations = this.locations;
+
+		if (hasProperFormat()) {
+			newLocations.forEach(function(loc) {
+				if (existingLocations.indexOf(loc) === -1) {
+					existingLocations.push(loc);
+				} else {
+					throw new Error('Location already exists');
+				}
+			});
+		} else {
+			throw new Error('Tried to add locations in wrong format');
+		}
+
+		return this;
+	}
+};
--- test/spec/plugin.js
+++ test/spec/plugin.js
@@ -6,19 +6,33 @@
   });
 
   describe('Timetable', function () {
+
     it('should by default set its scope from 9 to 17 hours', function () {
-      expect(this.timetable.scope).to.be.equal({hourStart: 9, hourEnd: 17});
+      expect(this.timetable.scope).to.deep.equal({hourStart: 9, hourEnd: 17});
     });
+
     it('should allow to set its scope between 0 and 24 hours', function () {
-      var outOfRange = new RangeError('Timetable scope must be between 0 and 24 hours (start, end)');
-      var badType = new TypeError('Timetable scope should consist of (start, end) in whole hours from 0 to 24');
-      expect(this.timetable.setScope(0, 24).scope).to.be.equal({hourStart: 0, hourEnd: 24});
-      expect(this.timetable.setScope(1, 20).scope).to.be.equal({hourStart: 1, hourEnd: 20});
-      expect(this.timetable.setScope(1, 25)).to.throw(outOfRange);
-      expect(this.timetable.setScope(-1, 1)).to.throw(outOfRange);
-      expect(this.timetable.setScope(1, 0)).to.throw(outOfRange);
-      expect(this.timetable.setScope(20)).to.throw(badType);
-      expect(this.timetable.setScope(undefined)).to.throw(badType);
+      var errorMsg = 'Timetable scope should consist of (start, end) in whole hours between 0 and 24';
+      var tt = this.timetable;
+      expect(tt.setScope(0, 24).scope).to.deep.equal({hourStart: 0, hourEnd: 24});
+      expect(tt.setScope(1, 20).scope).to.deep.equal({hourStart: 1, hourEnd: 20});
+      expect(function(){tt.setScope(1, 25);}).to.throw(RangeError, errorMsg);
+      expect(function(){tt.setScope(-1, 1);}).to.throw(RangeError, errorMsg);
+      expect(function(){tt.setScope(1, 0);}).to.throw(RangeError, errorMsg);
+      expect(function(){tt.setScope(20);}).to.throw(RangeError, errorMsg);
+      expect(function(){tt.setScope(undefined);}).to.throw(RangeError, errorMsg);
     });
+
+    it('should allow to add a list of locations', function () {
+      var tt = this.timetable;
+
+      expect(tt.addLocations(['loc1', 'loc2']).locations).to.deep.equal(['loc1', 'loc2']);
+      expect(tt.addLocations(['loc3']).locations).to.deep.equal(['loc1', 'loc2', 'loc3']);
+      expect(function(){tt.addLocations(['loc3']);}).to.throw('Location already exists');
+      expect(function(){tt.addLocations(undefined);}).to.throw('Tried to add locations in wrong format');
+      expect(function(){tt.addLocations({});}).to.throw('Tried to add locations in wrong format');
+    });
+
   });
+
 })();
