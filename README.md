8729e08f2dfc451b6a8cdb772eee629bea51a47d
steve <ssaanen@gmail.com>
2015-06-11 12:29:43 +0200

    added some tests for setting timetable scope

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # timetable.js
 Vanilla javascript plugin for building nice responsive timetables
+
+### not yet supported:
+- AM/PM time format for timetable header
--- test/spec/plugin.js
+++ test/spec/plugin.js
@@ -1,10 +1,24 @@
 (function () {
   'use strict';
 
-  describe('timetable.js', function () {
+  beforeEach(function() {
+    this.timetable = new Timetable();
+  });
 
-    it('should have a hello message', function () {
-      expect(new Timetable().message).to.be.equal('hello world!');
+  describe('Timetable', function () {
+    it('should by default set its scope from 9 to 17 hours', function () {
+      expect(this.timetable.scope).to.be.equal({hourStart: 9, hourEnd: 17});
+    });
+    it('should allow to set its scope between 0 and 24 hours', function () {
+      var outOfRange = new RangeError('Timetable scope must be between 0 and 24 hours (start, end)');
+      var badType = new TypeError('Timetable scope should consist of (start, end) in whole hours from 0 to 24');
+      expect(this.timetable.setScope(0, 24).scope).to.be.equal({hourStart: 0, hourEnd: 24});
+      expect(this.timetable.setScope(1, 20).scope).to.be.equal({hourStart: 1, hourEnd: 20});
+      expect(this.timetable.setScope(1, 25)).to.throw(outOfRange);
+      expect(this.timetable.setScope(-1, 1)).to.throw(outOfRange);
+      expect(this.timetable.setScope(1, 0)).to.throw(outOfRange);
+      expect(this.timetable.setScope(20)).to.throw(badType);
+      expect(this.timetable.setScope(undefined)).to.throw(badType);
     });
   });
 })();
